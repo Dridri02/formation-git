97a7766fed6cf9b1dd7f06175d0c58b2c42f73eb
Romain Warnan <romain.warnan@insee.fr>
2018-10-26 15:18:16 +0200

    Import inutile

--- src/main/java/fr/insee/bar/model/ActivityPojoJsonModule.java
+++ src/main/java/fr/insee/bar/model/ActivityPojoJsonModule.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
